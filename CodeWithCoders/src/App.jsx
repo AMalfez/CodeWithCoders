42180b24c7ae69cfa98374348755891b3abce26b
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-03-29 15:19:34 +0530

    added bootstrap sample navbar

---
+++ CodeWithCoders/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavigationBar from './components/Navigationbar'
+
+function App() {
+
+  return (
+    <>
+      <NavigationBar/>
+      HelloWorld
+    </>
+  )
+}
+
+export default App
