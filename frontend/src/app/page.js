85fece98b22b16ee554083aebc40e085ce4301a6
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-03-30 10:22:16 +0530

    added navbar

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Navigation from "@/components/navigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="min-h-screen">
+      <Navigation/>
+    {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
+     hi
     </main>
   );
 }
