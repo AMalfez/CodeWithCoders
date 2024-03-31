cb242961c56e54a0038c6331979d4d8a4a3d9829
Divya Raj <divyaraj7612@gmail.com>
2024-04-01 02:37:28 +0530

    rectified login page

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -5,7 +5,7 @@ import AuthContext from "@/context/authContext";
 import { redirect } from "next/navigation";
 
 const Login = () => {
-  const {setUser} = useContext(AuthContext)
+  const { setUser } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -25,7 +25,7 @@ const Login = () => {
       });
       if (response) {
         setUser(response.data.user);
-        return redirect('/dashboard')
+        return redirect("/dashboard");
       }
     } catch (e) {
       console.log(e);
@@ -56,7 +56,6 @@ const Login = () => {
                     setEmail(e.target.value);
                   }}
                   // autocomplete="email"
-                  onChange={(e)=>setEmail(e.target.value)}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -89,7 +88,7 @@ const Login = () => {
                   name="password"
                   type="password"
                   // autocomplete="current-password"
-                  onChange={(e)=>setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
