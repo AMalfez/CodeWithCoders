cf6116c0335616cc7a1b64d92d58ab12819dae51
AMalfez <a_mansuri@ch.iitr.ac.in>
2024-04-01 05:52:20 +0530

    payment working

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,12 +3,31 @@ import Navigation from "@/components/navbar";
 import AuthContext from "@/context/authContext";
 import { useState } from "react";
 import { useContext } from "react";
+import {loadStripe} from '@stripe/stripe-js'
 
 export default function Home() {
   const { templeData, user } = useContext(AuthContext);
-  // const [renderId, setRenderId] = useState(1);
+  const [renderId, setRenderId] = useState(1);
   const [templeToVisit, setTempleToVisit] = useState([]);
-
+const makePayment = async(temple)=>{
+  const stripe = await loadStripe('pk_test_51NHidkSCTsJY5ezVf5fpcafUVIq0ATRMxBUN8905m9fvSNV3jaZ9mbIKS41IFiOKe7zlYtbg2yIqUXFebRAyZShP00vTIBFHnS')
+  // const body = {
+  //   products: temple
+  // }
+  const header = {
+    "Content-Type":"application/json"
+  }
+  const response = await fetch('http://localhost:5000/payment/create-checkout-session',{
+    method:"POST",
+    headers:header,
+    body:JSON.stringify(temple)
+  })
+  const session = await response.json();
+  const result = stripe.redirectToCheckout({
+    sessionId:session.id
+  })
+  if(result.error) console.log(result.error);
+}
   const handleTempleSelection = (index) => {
     setRenderId(2);
     setTempleToVisit([templeData[index]]);
@@ -38,7 +57,7 @@ export default function Home() {
                   <span className="pr-2">{temple.location.country}</span>
                 </div>
                 <div>
-                  <button className="flex w-20 h-8 justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                  <button onClick={()=>makePayment({place:temple.name,price:100,date:new Date()})} className="flex w-20 h-8 justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
                     Book ticket
                   </button>
                 </div>
