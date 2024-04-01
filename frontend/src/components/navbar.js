cc8f1bfc7a36e28bb2bfe5c295952c72a711b52b
Divya Raj <divyaraj7612@gmail.com>
2024-04-01 05:49:03 +0530

    dashboard ui changed

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -105,8 +105,8 @@ function Navbar() {
         </div>
         <div>
           {user.length > 0 ? (
-            <div>
-              <div>{user[0].name}</div>
+            <div className="flex flex-row">
+              <div className="text-white mx-1">{user[0].name}</div>
               <img
                 className="h-8 w-8 rounded-full"
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
